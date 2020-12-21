4e2f2ed04c75eb164e691f4f6ecb5a32e9e72ebd
Florian Schroegendorfer <florian.schroegendorfer@phlo.at>
2020-12-21 02:45:50 +0100

    finished day 9

--- 2020/9/solve.hs
+++ 2020/9/solve.hs
@@ -1,22 +1,31 @@
 import Data.List
 import System.Environment
--- import Debug.Trace
 
-combinations 0 _  = [ [] ]
+combinations 0 _  = [[]]
 combinations k xs = [ y:ys | y:xs' <- tails xs
                            , ys <- combinations (k - 1) xs' ]
 
--- findInvalid xs | trace ("findInvalid " ++ show xs) False = undefined
-findInvalid ([], _) = error "empty preamble"
 findInvalid (_, []) = error "no more numbers"
-findInvalid (xs, (y:ys))
+findInvalid (xs, y:ys)
   | elem y (map sum (combinations 2 xs)) = findInvalid (tail xs ++ [y], ys)
   | otherwise = y
 
+findWeakness n (_, []) = error "no more numbers"
+findWeakness n (x0:x1:xs, y:ys)
+  | s == n = minimum xs' + maximum xs'
+  | s > n = findWeakness n ([x1, head xs], tail xs ++ y:ys)
+  | otherwise = findWeakness n (xs' ++ [y], ys)
+  where
+    xs' = x0:x1:xs
+    s = sum xs'
+
 main = do
-  [preamble, file] <- getArgs
-  content <- readFile file
-  let numbers = map (read :: String -> Int) (lines content)
+  [n, f] <- getArgs
+  raw <- readFile f
+  let numbers = map (read :: String -> Int) (lines raw)
   -- part 1
-  let invalid = findInvalid (splitAt (read preamble) numbers)
-  print $ invalid
+  let invalid = findInvalid (splitAt (read n) numbers)
+  print invalid
+  -- part 2
+  let weakness = findWeakness invalid (splitAt 2 numbers)
+  print weakness
